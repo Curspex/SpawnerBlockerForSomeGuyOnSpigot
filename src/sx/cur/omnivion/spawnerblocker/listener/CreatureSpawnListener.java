2998084d94c6dc958024fb2b35aadff95837fe29
8rrico <8rrico@gmail.com>
2015-06-02 09:52:58 -0400

    Refactor

--- src/sx/cur/omnivion/spawnerblocker/listener/CreatureSpawnListener.java
+++ src/sx/cur/omnivion/spawnerblocker/listener/CreatureSpawnListener.java
@@ -7,7 +7,7 @@ import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
 import org.bukkit.plugin.EventExecutor;
 
-public class CreatureSpawnListener  implements Listener, EventExecutor {
+public class CreatureSpawnListener implements Listener, EventExecutor {
 
 	@Override
 	public void execute(Listener listener, Event mevent) throws EventException
@@ -16,7 +16,9 @@ public class CreatureSpawnListener  implements Listener, EventExecutor {
 		
 		CreatureSpawnEvent event = (CreatureSpawnEvent) mevent;
 		
-		if (!event.getSpawnReason().equals(SpawnReason.SPAWNER)) event.setCancelled(true);
+		if (event.getSpawnReason().equals(SpawnReason.SPAWNER)) return;
+		
+		event.setCancelled(true);
 	}
 
 
