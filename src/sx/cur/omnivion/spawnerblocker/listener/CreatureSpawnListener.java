3ce7e42501889d69e73b9bc53f466944088e3a13
8rrico <8rrico@gmail.com>
2015-06-03 10:48:23 -0400

    Updates

--- src/sx/cur/omnivion/spawnerblocker/listener/CreatureSpawnListener.java
+++ src/sx/cur/omnivion/spawnerblocker/listener/CreatureSpawnListener.java
@@ -4,7 +4,6 @@ import org.bukkit.event.Event;
 import org.bukkit.event.EventException;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
-import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
 import org.bukkit.plugin.EventExecutor;
 
 public class CreatureSpawnListener implements Listener, EventExecutor {
@@ -12,13 +11,24 @@ public class CreatureSpawnListener implements Listener, EventExecutor {
 	@Override
 	public void execute(Listener listener, Event mevent) throws EventException
 	{
+
 		if (!(mevent instanceof CreatureSpawnEvent)) return;
-		
+
 		CreatureSpawnEvent event = (CreatureSpawnEvent) mevent;
-		
-		if (event.getSpawnReason().equals(SpawnReason.SPAWNER)) return;
-		
-		event.setCancelled(true);
+
+		switch (event.getSpawnReason())
+		{
+			case SPAWNER:
+			case SPAWNER_EGG:
+			case CUSTOM:
+			case SLIME_SPLIT:
+				break;
+
+			default:
+				event.setCancelled(true);
+				break;
+		}
+
 	}
 
 
